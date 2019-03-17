b3e39ce9f79fc18395af545564ddd38bd089bec3
Lima Vadakara <lima.vadakara@gmail.com>
2019-03-17 18:49:08 +0000

    Add logic to show roll values on frames of scorecard

--- src/BowlingController.js
+++ src/BowlingController.js
@@ -6,5 +6,26 @@ $(document).ready(function() {
     var pins = parseInt($('input:radio[name=rollPins]:checked').val(), 10);
     game.roll(pins);
     game.scoreGame();
+    var theFrame
+    for (var noFrames = 0; noFrames < game.frames.length; noFrames++){
+      theFrame = document.getElementById("frame"+noFrames);
+      while (theFrame.firstChild) {
+      theFrame.removeChild(theFrame.firstChild);
+      }
+    }
+
+    var rollDiv;
+    for (var framenumber = 0; framenumber< game.frames.length; framenumber++){
+
+      for(var rollnumber = 0; rollnumber< game.frames[framenumber].rolls.length;rollnumber++){
+        rollDiv = document.createElement('div');
+        rollDiv.id ="frame"+framenumber+"rollbox"+rollnumber;
+        rollDiv.className = "rollbox";
+        document.getElementById("frame"+framenumber).appendChild(rollDiv)
+
+        $('#frame'+framenumber+'rollbox'+rollnumber).text(game.frames[framenumber].rolls[rollnumber])
+
+      }
+    }
   }))
 })
